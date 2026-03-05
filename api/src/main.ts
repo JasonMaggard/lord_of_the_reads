1457646324303e98a54984bae93ac998f1d6c81a
JasonMaggard <lidjungle@gmail.com>
2026-03-05 00:02:19 -0500

    fix: generate prisma client on docker startup and enable graphql cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: false,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
